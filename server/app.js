8aa82db7047edff004e23456c9b88ce4056ffc00
Anup Malleswarapu <132218710+mvsanup17@users.noreply.github.com>
2025-06-30 10:11:46 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -24,17 +24,10 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ["https://project-mydata.netlify.app/"],
+    origin: "https://project-mydata.netlify.app/",
     credentials: true
 }))
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://project-mydata.netlify.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
 
 app.use(cookieParser())
 app.use(express.json())
